3e51ab8990a35eaaa692112e21a50edeb65e0e19
Aswatth <aswatth56@gmail.com>
2024-07-26 16:29:47 +0530

    Update file_io_test.go

--- utils/file_handler/file_io_test.go
+++ utils/file_handler/file_io_test.go
@@ -1,68 +1,41 @@
 package file_handler
 
 import (
-	"encoding/json"
-	"ncrypt/models"
+	"os"
 	"testing"
 )
 
 func TestSave(t *testing.T) {
+	file_name := "save_test.txt"
 
-	file_name := "test.txt"
+	data_to_save := "Hi there. This is a test"
 
-	contents := []models.Content{{Name: "item_2", Value: "secret"}}
-	data_list := []models.Data{{Name: "test_data", Contents: contents}}
-
-	data_as_bytes, _ := json.Marshal(data_list)
-
-	err := Save(file_name, data_as_bytes)
+	err := Save(file_name, []byte(data_to_save))
 
 	if err != nil {
 		t.Errorf("Saving failed")
 		return
 	}
+
+	os.Remove("save_test.txt")
 }
 
 func TestRead(t *testing.T) {
+	file_name := "read_test.txt"
 
-	TestSave(t)
+	data_to_save := "Hi there. This is a test"
 
-	file_name := "test.txt"
-
-	contents := []models.Content{{Name: "item_2", Value: "secret"}}
-	expected_data_list := []models.Data{{Name: "test_data", Contents: contents}}
+	Save(file_name, []byte(data_to_save))
 
 	fetched_data, err := Read(file_name)
 
 	if err != nil {
 		t.Errorf("Reading failed")
-		return
 	}
 
-	var actual_data_list []models.Data
-
-	json.Unmarshal(fetched_data, &actual_data_list)
-
-	if len(expected_data_list) != len(actual_data_list) {
-		t.Errorf("Record count mismatch")
-		return
+	if string(fetched_data[:]) != data_to_save {
+		t.Errorf("Incorrect data")
 	}
 
-	for i := range len(expected_data_list) {
-		if expected_data_list[i].Name != actual_data_list[i].Name {
-			t.Errorf("Mismatch in data")
-			return
-		}
-		if len(expected_data_list[i].Contents) != len(actual_data_list[i].Contents) {
-			t.Errorf("Content count mismatch")
-			return
-		}
-
-		for j := range expected_data_list[i].Contents {
-			if (expected_data_list[i].Contents[j].Name != actual_data_list[i].Contents[j].Name) || (expected_data_list[i].Contents[j].Value != actual_data_list[i].Contents[j].Value) {
-				t.Errorf("Mismatch in content")
-				return
-			}
-		}
-	}
+	os.Remove("read_test.txt")
 }
