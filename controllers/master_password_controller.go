7fba326cf5f1d758cfb37683b55899ca850ef6df
Aswatth <aswatth56@gmail.com>
2024-07-29 21:05:58 +0530

    Created master_password_controller

---
+++ controllers/master_password_controller.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"ncrypt/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type MasterPasswordController struct {
+	service services.MasterPasswordService
+}
+
+func (obj *MasterPasswordController) Init(service *services.MasterPasswordService) {
+	obj.service = *service
+}
+
+func (obj *MasterPasswordController) SetPassword(ctx *gin.Context) {
+	var data map[string]string
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+		return
+	} else {
+		obj.service.SetMasterPassword(data["master_password"])
+	}
+}
+
+func (obj *MasterPasswordController) Validate(ctx *gin.Context) {
+	var data map[string]string
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+		return
+	} else {
+		result, err := obj.service.ValidateMasterPassword(data["master_password"])
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		if !result {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, "invalid password")
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	}
+}
+
+func (obj *MasterPasswordController) RegisterRoutes(rg *gin.RouterGroup) {
+	group := rg.Group("master_password")
+
+	group.POST("", obj.SetPassword)
+	group.GET("", obj.Validate)
+}
