cbc1d7e4776fe868f69c96b05a7b093db8d38702
Aswatth <aswatth56@gmail.com>
2024-08-10 10:32:52 +0530

    Update i_database.go

--- utils/database/i_database.go
+++ utils/database/i_database.go
@@ -3,7 +3,7 @@ package database
 type IDatabase interface {
 	SetDatabase(database_name string) error
 	GetData(table_name string, params ...string) (interface{}, error)
-	GetAllData(table_name string, params ...string) ([]interface{}, error)
+	GetAllData(params ...string) ([]interface{}, error)
 	AddData(table_name string, data interface{}) error
 	UpdateData(table_name string, data interface{}, params ...string) error
 	DeleteData(table_name string, params ...string) error
