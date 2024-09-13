ac5bd05f1783959a12d6730b46bfd6649200727a
Aswatth <aswatth56@gmail.com>
2024-09-13 13:12:55 -0400

    Removed unused files.

--- models/content.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Content struct {
-	Name string `json:"name" bson:"name"`
-	Value string `json:"value" bson:"value"`
-}
--- models/data.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Data struct {
-	Name string `json:"name" bson:"name"`
-	Contents []Content `json:"contents" bson:"contents"`
-}
